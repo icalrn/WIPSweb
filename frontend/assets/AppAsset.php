284eec8a00dfee822d2e499a2f2f606852f33f94
Faisal Rahman <id.faisalrahman@gmail.com>
2015-05-26 14:30:39 +0700

    added Full Page js to frontend asset

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -19,8 +19,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/jquery.fullPage.css',
     ];
     public $js = [
+        'js/jquery.fullPage.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
